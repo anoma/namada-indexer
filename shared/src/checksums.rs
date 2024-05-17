db2cb4307d76876d9d1d28decc9740766ee80ece
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-05-17 13:46:20 +0200

    wip

--- shared/src/checksums.rs
+++ shared/src/checksums.rs
@@ -9,16 +9,6 @@ use serde::Deserialize;
 pub struct Checksums(BiMap<String, String>);
 
 impl Checksums {
-    pub fn init(&mut self) {
-        let mut clean_checksums = BiMap::default();
-        for (key, value) in self.0.clone() {
-            let key = key.strip_suffix(".wasm").unwrap().to_owned();
-            let value = value.split('.').nth(1).unwrap().to_owned();
-            clean_checksums.insert(key, value);
-        }
-        self.0 = clean_checksums;
-    }
-
     pub fn get_name_by_id(&self, hash: &str) -> Option<String> {
         self.0.get_by_right(hash).map(|data| data.to_owned())
     }
