f07b424a55bc07f723f729b0dc77dc6bf65743d0
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2024-06-13 15:02:12 +0200

    feat: tx improvements (#57)

--- orm/migrations/2024-06-09-102302_transactions/down.sql
+++ orm/migrations/2024-06-09-102302_transactions/down.sql
@@ -1,4 +1,7 @@
 -- This file should undo anything in `up.sql`
+DROP TABLE IF EXISTS inner_transactions;
+
 DROP TABLE IF EXISTS wrapper_transactions;
 
-DROP TABLE IF EXISTS inner_transactions;
+DROP TYPE TRANSACTION_KIND;
+DROP TYPE TRANSACTION_RESULT;
