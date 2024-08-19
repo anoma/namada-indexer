264a6176834b86fb27e0bd53e28baa67e68c4e04
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2024-08-19 09:14:45 +0200

    fix: one zero removed from hardcoded gas limits

--- orm/migrations/2024-06-12-140948_gas/up.sql
+++ orm/migrations/2024-06-12-140948_gas/up.sql
@@ -8,46 +8,46 @@ CREATE TABLE gas (
 ALTER TABLE gas ADD UNIQUE (tx_kind, token);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('transparent_transfer', 'native', 1000000);
+VALUES ('transparent_transfer', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('shielded_transfer', 'native', 1000000);
+VALUES ('shielded_transfer', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('shielding_transfer', 'native', 1000000);
+VALUES ('shielding_transfer', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('unshielding_transfer', 'native', 1000000);
+VALUES ('unshielding_transfer', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('bond', 'native', 1000000);
+VALUES ('bond', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('redelegation', 'native', 1000000);
+VALUES ('redelegation', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('unbond', 'native', 1000000);
+VALUES ('unbond', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('withdraw', 'native', 1000000);
+VALUES ('withdraw', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('claim_rewards', 'native', 1000000);
+VALUES ('claim_rewards', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('vote_proposal', 'native', 1000000);
+VALUES ('vote_proposal', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('init_proposal', 'native', 1000000);
+VALUES ('init_proposal', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('change_metadata', 'native', 1000000);
+VALUES ('change_metadata', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('change_commission', 'native', 1000000);
+VALUES ('change_commission', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('reveal_pk', 'native', 1000000);
+VALUES ('reveal_pk', 'native', 100000);
 
 INSERT INTO gas (tx_kind, token, gas_limit)
-VALUES ('unknown', 'native', 1000000);
+VALUES ('unknown', 'native', 100000);
