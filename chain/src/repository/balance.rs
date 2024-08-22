2b76c49857435f407d94e796b5c5e879f31669d0
Joel Nordell <joel@unit410.com>
2024-08-22 17:41:46 -0500

    [chain] When inserting balances, do it in chunks to avoid hitting query limits

--- chain/src/repository/balance.rs
+++ chain/src/repository/balance.rs
@@ -8,21 +8,32 @@ pub fn insert_balance(
     transaction_conn: &mut PgConnection,
     balances: Balances,
 ) -> anyhow::Result<()> {
-    diesel::insert_into(balance_changes::table)
-        .values::<&Vec<BalanceChangesInsertDb>>(
-            &balances
-                .into_iter()
-                .map(BalanceChangesInsertDb::from_balance)
-                .collect::<Vec<_>>(),
-        )
-        .on_conflict((
-            balance_changes::columns::owner,
-            balance_changes::columns::token,
-            balance_changes::columns::height,
-        ))
-        .do_nothing()
-        .execute(transaction_conn)
-        .context("Failed to update balances in db")?;
+    tracing::info!("Inserting {} balances into db", balances.len());
+
+    // Group balances into chunks to avoid hitting the limit of the number of bind parameters in one query.
+    balances
+        .chunks(10000)
+        .into_iter()
+        .try_for_each(|balances_chunk| {
+            diesel::insert_into(balance_changes::table)
+                .values::<&Vec<BalanceChangesInsertDb>>(
+                    &balances_chunk
+                        .to_vec()
+                        .into_iter()
+                        .map(BalanceChangesInsertDb::from_balance)
+                        .collect::<Vec<_>>(),
+                )
+                .on_conflict((
+                    balance_changes::columns::owner,
+                    balance_changes::columns::token,
+                    balance_changes::columns::height,
+                ))
+                .do_nothing()
+                .execute(transaction_conn)
+                .context("Failed to update balances in db")?;
+
+            anyhow::Ok(())
+        })?;
 
     anyhow::Ok(())
 }
