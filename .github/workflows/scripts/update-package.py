7dee869fb9465ff9d62309ddc74207678a223261
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-12-03 15:34:26 +0100

    improve ci

--- .github/workflows/scripts/update-package.py
+++ .github/workflows/scripts/update-package.py
@@ -1,15 +1,12 @@
 import json 
 import sys
 
-package_json_path = sys.argv[1]
-package_version = sys.argv[2]
+package_version = sys.argv[1]
 
-package_json = json.load(open(package_json_path))
+configuration = {
+  "npmName": "@namada/indexer-client",
+  "npmVersion": package_version
+}
 
-package_json['name'] = "namada-indexer-client"
-package_json['version'] = package_version
-package_json['description'] = "Set of API to interact with a namada indexer."
-package_json['license'] = "GPL-3.0 license"
-
-with open(package_json_path, 'w', encoding='utf-8') as f:
-    json.dump(package_json, f, ensure_ascii=False, indent=4)
+with open("swagger-codegen.json", 'w+', encoding='utf-8') as f:
+    json.dump(configuration, f, ensure_ascii=False, indent=4)
