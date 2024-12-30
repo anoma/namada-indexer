849049484afcdbb986e9a2df6eb8bd21ffe77481
neocybereth <sean@arrowburn.co.nz>
2024-12-31 09:10:26 +1300

    fix: update sql statements

--- orm/migrations/2024-12-16-231256_add_epoch_pos_rewards_table/down.sql
+++ orm/migrations/2024-12-16-231256_add_epoch_pos_rewards_table/down.sql
@@ -1,1 +1,3 @@
+ALTER table pos_rewards ADD CONSTRAINT pos_rewards_owner_validator_id_key UNIQUE (owner, validator_id);
+ALTER table pos_rewards DROP CONSTRAINT pos_rewards_owner_validator_id_epoch_key;
 ALTER TABLE pos_rewards DROP COLUMN epoch;
--- orm/migrations/2024-12-16-231256_add_epoch_pos_rewards_table/up.sql
+++ orm/migrations/2024-12-16-231256_add_epoch_pos_rewards_table/up.sql
@@ -1,6 +1,7 @@
--- Alters the pos_rewards table to add a fourth column:
--- epoch: INTEGER, representing the epoch number
 -- Populate existing records with epoch = 0
 ALTER TABLE pos_rewards ADD COLUMN epoch INTEGER NOT NULL DEFAULT 0;
 -- Now we can safely drop the default
 ALTER TABLE pos_rewards ALTER COLUMN epoch DROP DEFAULT;
+-- Also update the UNIQUE constraint to include the epoch column
+ALTER table pos_rewards ADD CONSTRAINT pos_rewards_owner_validator_id_epoch_key unique (owner, validator_id, epoch);
+ALTER table pos_rewards DROP CONSTRAINT pos_rewards_owner_validator_id_key;
