981e3dc6deb22f72545cdbc0c3c9fa2962c19f90
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2025-01-16 06:22:38 +0100

    added orm entities

---
+++ transactions/src/repository/masp.rs
@@ -0,0 +1,22 @@
+use anyhow::Context;
+use diesel::{PgConnection, RunQueryDsl};
+use orm::masp::MaspInsertDb;
+use orm::schema::masp_pool;
+use shared::masp::MaspEntry;
+
+pub fn insert_masp_entries(
+    transaction_conn: &mut PgConnection,
+    masp_entries: Vec<MaspEntry>,
+) -> anyhow::Result<()> {
+    diesel::insert_into(masp_pool::table)
+        .values::<&Vec<MaspInsertDb>>(
+            &masp_entries
+                .into_iter()
+                .map(MaspInsertDb::from)
+                .collect::<Vec<_>>(),
+        )
+        .execute(transaction_conn)
+        .context("Failed to insert masp pool entries in db")?;
+
+    anyhow::Ok(())
+}
