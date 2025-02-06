9372c4dc448786db152c040b7f263691844f6052
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-02-06 11:28:09 +0000

    generalize container fut mechanism

--- shared/src/futures.rs
+++ shared/src/futures.rs
@@ -1,24 +1,53 @@
 use std::future::Future;
 
-pub trait AwaitOption<F> {
-    type Output;
+pub trait AwaitContainer<F> {
+    type Output<T>;
 
     #[allow(async_fn_in_trait)]
-    async fn future(self) -> Option<Self::Output>;
+    async fn future<T>(self) -> Self::Output<F::Output>
+    where
+        F: Future<Output = T>;
 }
 
-impl<F, T> AwaitOption<F> for Option<F>
-where
-    F: Future<Output = T>,
-{
-    type Output = T;
+impl<F> AwaitContainer<F> for Option<F> {
+    type Output<X> = Option<X>;
 
     #[inline]
-    async fn future(self) -> Option<T> {
+    async fn future<T>(self) -> Option<T>
+    where
+        F: Future<Output = T>,
+    {
         if let Some(fut) = self {
             Some(fut.await)
         } else {
             None
         }
+    }
+}
+
+impl<F, E> AwaitContainer<F> for Result<F, E> {
+    type Output<X> = Result<X, E>;
+
+    #[inline]
+    async fn future<T>(self) -> Result<T, E>
+    where
+        F: Future<Output = T>,
+    {
+        match self {
+            Ok(fut) => Ok(fut.await),
+            Err(err) => Err(err),
+        }
+    }
+}
+
+impl<F> AwaitContainer<F> for Vec<F> {
+    type Output<X> = Vec<X>;
+
+    #[inline]
+    async fn future<T>(self) -> Vec<T>
+    where
+        F: Future<Output = T>,
+    {
+        ::futures::future::join_all(self).await
     }
 }
