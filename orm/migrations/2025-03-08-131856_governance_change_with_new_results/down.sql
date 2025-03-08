8df1824e68b093197dfa4628ce9483942f01bc08
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2025-03-08 14:26:34 +0100

    missing governance migrations

---
+++ orm/migrations/2025-03-08-131856_governance_change_with_new_results/down.sql
@@ -0,0 +1,51 @@
+UPDATE governance_proposals
+SET result = 'rejected'
+WHERE id = 0;
+
+UPDATE governance_proposals
+SET result = 'rejected'
+WHERE id = 1;
+
+UPDATE governance_proposals
+SET result = 'rejected'
+WHERE id = 2;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 3;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 4;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 5;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 6;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 7;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 8;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 9;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 10;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 11;
+
+UPDATE governance_proposals
+SET result = 'passed'
+WHERE id = 12;
