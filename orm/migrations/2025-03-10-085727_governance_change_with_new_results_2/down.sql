ba41e3c1c923b06c47e145cde6dd34e0105cbf7d
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2025-03-11 09:46:30 +0100

    gov: remove invalid migrations, fix executed proposal status

--- orm/migrations/2025-03-10-085727_governance_change_with_new_results_2/down.sql
+++
@@ -1,3 +0,0 @@
-UPDATE governance_proposals
-SET result = 'passed'
-WHERE id = 13;
--- orm/migrations/2025-03-10-085727_governance_change_with_new_results_2/up.sql
+++
@@ -1,4 +0,0 @@
--- Your SQL goes here
-UPDATE governance_proposals
-SET result = 'executed_passed'
-WHERE id = 13;
